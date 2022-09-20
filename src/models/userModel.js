8a5aed2be4ab7f380cb1b48609e9180bc7cbb0da
saurabhshete100X <saurabhshete281@gmail.com>
2022-09-20 13:17:53 +0530

    User Model Complete

---
+++ src/models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+
+        title: {
+            type: String,
+            required: true,
+            enum: [Mr, Mrs, Miss],
+            trim: true
+        },
+
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minLen: 8, maxLen: 15
+        },
+        address: {
+            street: { type: String },
+            city: { type: String },
+            pincode: { type: String }
+        },
+
+    }, { timestamps: true }
+
+)
+
+module.exports = mongoose.model("user", userSchema)
