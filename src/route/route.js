dd1e79c2f42364e498aa4a617445d71ba5119204
Isha Jaiswal <jaiswalisha172@gmail.com>
2022-09-21 13:33:31 +0530

    project3

--- src/route/route.js
+++ src/route/route.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const route = express.Router()
 const {createUser, userLogin} = require("../controllers/userController")
+const getbooksbyid = require("../controllers/bookController")
 
 
 route.post("/register" , createUser)
 
 route.post("/login" , userLogin)
+
+
 
 
 
